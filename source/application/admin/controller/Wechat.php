822b946043c1caaa62c707e27f0411e5c7c84d5e
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-11-18 23:18:10 +0800

    test

--- source/application/admin/controller/Wechat.php
+++ source/application/admin/controller/Wechat.php
@@ -2,7 +2,7 @@
 
 namespace app\admin\controller;
 
-
+use EasyWeChat\Factory;
 /**
  * 微信测试
  * Class Index
@@ -16,7 +16,27 @@ class Wechat extends Controller
      */
     public function index()
     {
-        return $this->fetch('index');
+
+
+        $config = [
+            'app_id' => 'wxc4d225cdc68bab01',
+            'secret' => 'c772e094ab6d7091db037573394920b4',
+            'token' => 'X9VfZef6ntpPySDT',
+            'response_type' => 'array',
+            //...
+        ];
+
+        var_dump($config);
+
+        $app = Factory::officialAccount($config);
+
+        var_dump($app);
+
+        $response = $app->server->serve();
+
+        var_dump($response);
+// 将响应输出
+        $response->send();exit;
     }
 
 }
