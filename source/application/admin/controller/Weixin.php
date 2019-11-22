7521b63d86ac315ad6a08e555f1687527be29ed5
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-11-22 09:23:31 +0800

    响应文本消息

--- source/application/admin/controller/Weixin.php
+++ source/application/admin/controller/Weixin.php
@@ -10,25 +10,60 @@ use app\common\library\wechat;
  */
 class Weixin extends \think\Controller
 {
+    private $config = [
+        'app_id' => 'wxc4d225cdc68bab01',
+        'secret' => 'c772e094ab6d7091db037573394920b4',
+        'token' => 'X9VfZef6ntpPySDT',
+        'response_type' => 'array',
+        //...
+    ];
     /**
-     * 后台首页
+     * 验证签名
      * @return mixed
      */
     public function valid()
     {
-
-        $config = [
-            'app_id' => 'wxc4d225cdc68bab01',
-            'secret' => 'c772e094ab6d7091db037573394920b4',
-            'token' => 'X9VfZef6ntpPySDT',
-            'response_type' => 'array',
-            //...
-        ];
-
-        $wx = new wechat\WxSdk($config['app_id'],$config['secret'],$config['token']);
-
+        $wx = new wechat\WxSdk(
+            $this->config['app_id'],
+            $this->config['secret'],
+            $this->config['token']);
         $wx->valid();
-
     }
+
+    // 响应文本消息
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+            if($keyword == "?" || $keyword == "？")
+            {
+                $msgType = "text";
+                $contentStr = date("Y-m-d H:i:s",time());
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                //file_put_contents("log.txt",date('Y-m-d H:i:s').var_export($resultStr,true). "\n",FILE_APPEND);
+                echo $resultStr;
+            }
+        }else{
+            echo "";
+            exit;
+        }
+    }
+
+
+
 
 }
