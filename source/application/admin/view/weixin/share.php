d37b5999c113932ddd441d9d0fc1dc918248f982
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-11-23 10:05:06 +0800

    new share

--- source/application/admin/view/weixin/share.php
+++ source/application/admin/view/weixin/share.php
@@ -90,61 +90,6 @@
 
         });
 
-
-
-        //自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
-        wx.ready(function(){
-            wx.onMenuShareTimeline({
-                title: weixinShareShareTitle,
-                link: weixinShareLineLink,
-                imgUrl: weixinShareImgUrl,
-                trigger: function (res) {
-                    // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                    alert('用户点击分享到朋友圈');
-                },
-                success: function (res) {
-                    alert('已分享');
-                },
-                cancel: function (res) {
-                    alert('已取消');
-                },
-                fail: function (res) {
-                    alert(JSON.stringify(res));
-                }
-            });
-            wx.onMenuShareAppMessage({
-                title: weixinShareShareTitle,
-                link: weixinShareLineLink,
-                desc: weixinShareDescContent,
-                imgUrl: weixinShareImgUrl,
-                trigger: function (res) {
-                    // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                    alert('用户点击发送给朋友');
-                },
-                success: function (res) {
-                    alert('已分享');
-                },
-                cancel: function (res) {
-                    alert('已取消');
-                },
-                fail: function (res) {
-                    alert(JSON.stringify(res));
-                }
-            });
-            wx.hideMenuItems({
-                menuList: [
-                    'menuItem:readMode', // 阅读模式
-                    'menuItem:share:timeline', // 分享到朋友圈
-                    'menuItem:copyUrl' // 复制链接
-                ],
-                success: function (res) {
-                    alert('已隐藏“阅读模式”，“分享到朋友圈”，“复制链接”等按钮');
-                },
-                fail: function (res) {
-                    alert(JSON.stringify(res));
-                }
-            });
-        });
     </script>
     <style>
     .test {
